d90e3b1e44979b1a3155ebe0a7bc92ea20f06354
Nathan Farias <nathan90055@gmail.com>
2025-05-22 22:32:36 -0300

    FEAT: Criar BaseEntity para as entidades

--- Domain/Entities/Movie.cs
+++ Domain/Entities/Movie.cs
@@ -9,32 +9,19 @@ namespace Domain.Entities
 {
     public class Movie
     {
-        /// <summary>
-        /// Lista de URLs das imagens associadas ao filme.
-        /// </summary>
         public virtual ICollection<MovieImage> Images { get; set; }
 
-        /// <summary>
-        /// Coleção de prêmios recebidos pelo filme.
-        /// </summary>
         public virtual ICollection<Award> Awards { get; set; }
 
-        /// <summary>
-        /// Sinopse do filme, contendo um resumo da história.
-        /// </summary>
         public string Synopsis { get; set; }
 
         public MovieImage? Poster { get; private set; }
 
-        /// <summary>
-        /// Ano de lançamento do filme.
-        /// </summary>
         public int ReleaseYear { get; set; }
 
-        /// <summary>
-        /// Duração do filme em minutos.
-        /// </summary>
-        public int Duration { get; set; }
+        public Duration Duration { get; set; }
+
+
         public bool IsActive { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime UpdatedAt { get; private set; }
---
+++ Domain/SeedWork/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace MoviesAPIAdminModule.Domain.SeedWork
+{
+    /// <summary>
+    /// Representa a entidade base para todas as entidades do domínio, fornecendo um identificador único.
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+
+        public string Name { get; set; }
+
+        protected BaseEntity() => Id = Guid.NewGuid();
+    }
+}
