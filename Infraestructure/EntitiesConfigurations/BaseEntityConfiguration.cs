7f336e5f89917a612f8f8cb4029cbd6a01f65093
N4thann <nathan90055@gmail.com>
2025-06-17 16:42:45 -0300

    FEAT: Criar através do FluentAPI as configurações de BaseEntity e Director

---
+++ Infraestructure/EntitiesConfigurations/BaseEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoviesAPIAdminModule.Domain.SeedWork;
+
+
+namespace Infraestructure.EntitiesConfigurations
+{
+    public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T>
+        where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder) 
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+        }
+    }
+}
---
+++ Infraestructure/EntitiesConfigurations/DirectorConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infraestructure.EntitiesConfigurations
+{
+    public class DirectorConfiguration : BaseEntityConfiguration<Director>
+    {
+        public override void Configure(EntityTypeBuilder<Director> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(d => d.Biography)
+                .HasMaxLength(1000);
+
+            builder.Property(d => d.BirthDate)
+                .IsRequired()
+                .HasColumnType("date");
+
+            builder.Property(d => d.Gender)
+                .IsRequired()
+                .HasConversion<string>();// Armazena o enum como string
+
+            builder.Property(d => d.IsActive)
+                .IsRequired();
+
+            builder.Property(d => d.CreatedAt)
+                .IsRequired();
+
+            builder.Property(d => d.UpdatedAt)
+                .IsRequired();
+
+            builder.OwnsOne(d => d.Country, countryBuilder =>
+            {
+                countryBuilder.Property(c => c.Name)
+                              .HasColumnName("CountryName")
+                              .HasMaxLength(100)
+                              .IsRequired();
+
+                countryBuilder.Property(c => c.Code)
+                              .HasColumnName("CountryCode")
+                              .HasMaxLength(3)
+                              .IsRequired();
+            });
+        }
+    }
+}
