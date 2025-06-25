b9212f19a108c71862ec08c64e464fdff9bd0839
N4thann <nathan90055@gmail.com>
2025-06-24 23:49:35 -0300

    FEAT: Criar repositório de Studio com método AddAsync

---
+++ Infraestructure/Repository/StudioRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Domain.SeedWork.Interfaces;
+using Infraestructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository
+{
+    public class StudioRepository : IStudioRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudioRepository(ApplicationDbContext context) => _context = context;
+
+        public async Task AddAsync(Studio studio)
+        {
+            await _context.Studios.AddAsync(studio);
+        }
+    }
+}
